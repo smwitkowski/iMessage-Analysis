c5aa9f46cb7c707e344009bb1e982527aa04bf34
Stephen Witkowski <stephen.witkowski@protonmail.com>
2021-03-07 08:28:48 -0600

    Wrapped inside __main__

--- src/data/collect_imessage_data.py
+++ src/data/collect_imessage_data.py
@@ -1,17 +1,19 @@
 import sqlite3
 import csv
 
-conn = sqlite3.connect("/Users/switkowski/PycharmProjects/iMessages/data/raw/chat.db")
-cur = conn.cursor()
 
-tables = []
-for table in cur.execute("""SELECT name FROM sqlite_master WHERE type='table' ORDER BY name;"""):
-    tables.append(table[0])
+if __name__ == "__main__":
+    conn = sqlite3.connect("/Users/switkowski/PycharmProjects/iMessages/data/raw/chat.db")
+    cur = conn.cursor()
 
-for table in tables:
-    data = cur.execute(f'SELECT * FROM {table}')
-    columns = [d[0] for d in data.description]
-    with open('/Users/switkowski/PycharmProjects/iMessages/data/raw/{}.csv'.format(table), 'w') as f:
-        writer = csv.writer(f)
-        writer.writerow(columns)
-        writer.writerows(data)
+    tables = []
+    for table in cur.execute("""SELECT name FROM sqlite_master WHERE type='table' ORDER BY name;"""):
+        tables.append(table[0])
+
+    for table in tables:
+        data = cur.execute(f'SELECT * FROM {table}')
+        columns = [d[0] for d in data.description]
+        with open('/Users/switkowski/PycharmProjects/iMessages/data/raw/{}.csv'.format(table), 'w') as f:
+            writer = csv.writer(f)
+            writer.writerow(columns)
+            writer.writerows(data)
